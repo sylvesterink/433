ec84bb40a845762ebc34dc4e831dea14cab24bbe
Sylvester Ink <sylvesterink@gmail.com>
2012-02-16 20:39:56 -0800

    Finished tokenizer, implemented basic execution

--- project02/main.cpp
+++ project02/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 #include <string>
 #include <sstream>
 #include <cstring>
@@ -9,46 +10,45 @@
 using namespace std;
 
 void getCommand();
-void processCommand(string&, char**);
+int processCommand(string&, char**);
 void execCommand();
 void runProcess();
 
 const int MAX_PARAMETERS = 125;
-const int MAX_LINE_SIZE = 256;
 
 int main(int argc, const char* argv[])
 {
+    cout << "Myshell" << endl;
 
-  cout << "Myshell" << endl;
-  char input[MAX_LINE_SIZE];
-  string inputString = "";
-  char** command = NULL;
-  for(int j = 0; j < MAX_PARAMETERS; j++){
-    command[j] = "";
-  }
-  
-  while(true){
-    cin.getline(input, 256);
-    if(strcmp(input, "exit") == 0)
-      exit(0);
-    inputString = input;
-    processCommand(inputString, command);
+    string inputString = "";
+    char* parameters[MAX_PARAMETERS];
+    int numParams = 0;
 
-    //execv("/bin/ls", command);
-    //for(int i = 0; i < 125; i++) {
-    //  cout << command[i] << endl;
-    // }
-    cout << command[0] << endl;
-  }//end while
+    bool loop = true;
 
+    while(loop){
+        getline(cin, inputString);
+        if( inputString == "exit") {
+            loop = false;
+        }
+        else {
+            numParams = processCommand(inputString, parameters);
 
+            //for (int i = 0; i < 6; i++) {
+                //cout << parameters[i] << endl;
+            //}
+            execvp(parameters[0], parameters);
+        }
+    }//end while
 
+    //if(command != NULL) {
+        //for (int i = 0; i < numParams; i++) {
+            //delete[] command[i];
+        //}
+        //delete[] command;
+    //}
 
-
-  if(commandList != NULL)
-    delete *commandList;
-
-  return 0;
+    return 0;
 }
 
 
@@ -63,48 +63,55 @@ void getCommand()
 }
 
 
-void processCommand(string &rawCommand, char** commandList)
+int processCommand(string &rawCommand, char** commandList)
 {
-  istringstream convertInput(rawCommand);
-  vector<string> commandVector;
-  string temp = "";
-  //int counter = 0;
-  while( getline(convertInput, temp, ' ') ){
-    commandVector.add(temp);
-  }
-  *commandList = new char[commandVector.size() + 1]; 
-  for(int i = 0; i < commandVector.size() + 1; i++) {
-    commandList[i] = commandVector[i].c_str();
+    istringstream convertInput(rawCommand);
+
+    string temp = "";
+    // count number of parameters
+    int parameterCount = 0;
+
+    while( getline(convertInput, temp, ' ') &&
+            (parameterCount < MAX_PARAMETERS - 1) ){
+
+        commandList[parameterCount] = new char[temp.size()];
+        strcpy(commandList[parameterCount], temp.c_str());
+        parameterCount++;
+    }
+
+    commandList[parameterCount] = NULL;
+
+    return parameterCount;
 }
 
-void execCommand()
-{
+    void execCommand()
+    {
 
 
-}
+    }
 
 
-void runProcess()
-{
-  pid_t  pid;
-  /* fork another process */
-  pid = fork();
+    void runProcess()
+    {
+        pid_t  pid;
+        /* fork another process */
+        pid = fork();
 
-  if (pid < 0) { /* error occurred */
-    cerr << "Fork Failed" << endl;
-    exit(-1);
-  }
-  else if (pid == 0) { /* child process */
-    cout << "executing child" << endl;
-    execlp("/bin/ls", "ls", NULL);
-  }
-  else { /* parent process */
-      /* parent will wait for the child to complete */
-      wait();
-      cout << "Child Complete" << endl;
-      //exit(0);
-  }
+        if (pid < 0) { /* error occurred */
+            cerr << "Fork Failed" << endl;
+            exit(-1);
+        }
+        else if (pid == 0) { /* child process */
+            cout << "executing child" << endl;
+            execlp("/bin/ls", "ls", NULL);
+        }
+        else { /* parent process */
+            /* parent will wait for the child to complete */
+            wait();
+            cout << "Child Complete" << endl;
+            //exit(0);
+        }
 
 
 
-}
+    }
