5cf1bc88719d08d541d47126fdf756498aeb47ec
Sylvester Ink <sylvesterink@gmail.com>
2012-02-24 03:28:30 -0800

    Finished implementing local commands.

--- project02/main.cpp
+++ project02/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 #include <string>
 #include <sstream>
+
 #include <cstring>
 #include <cstdlib>
-#include <iterator>
+#include <errno.h>
 
 using namespace std;
 
-void getCommand();
 int parseCommand(string&, char**);
-bool runLocalProcess(char**);
+bool runLocalProcess(char**, vector<string>&);
+void cmdPushd(char*, vector<string>&);
+void cmdPopd(vector<string>&);
+void cmdDirs(vector<string>&);
+bool cmdCd(const char*);
+
 void runProcess(char**);
 
 const int MAX_PARAMETERS = 125;
@@ -23,6 +27,8 @@ int main(int argc, const char* argv[])
     string inputString = "";
     char* parameters[MAX_PARAMETERS];
     int numParams = 0;
+
+    vector<string> directoryStack;
 
     bool loop = true;
 
@@ -37,7 +43,7 @@ int main(int argc, const char* argv[])
         else {
             numParams = parseCommand(inputString, parameters);
 
-            if (!runLocalProcess(parameters)) {
+            if (!runLocalProcess(parameters, directoryStack)) {
                 runProcess(parameters);
             }
 
@@ -54,11 +60,6 @@ int main(int argc, const char* argv[])
     }
 
     return 0;
-}
-
-void getCommand()
-{
-
 }
 
 int parseCommand(string &rawCommand, char** commandList)
@@ -82,43 +83,106 @@ int parseCommand(string &rawCommand, char** commandList)
     return parameterCount;
 }
 
-bool runLocalProcess(char** commandList)
+//TODO: add if statement to test if commandlist[1] is null
+bool runLocalProcess(char** commandList, vector<string>& directoryStack)
 {
-
     if ( strcmp(commandList[0], "pushd") == 0 ) {
+        cmdPushd(commandList[1], directoryStack);
         return true;
     }
     else if ( strcmp(commandList[0], "popd") == 0 ) {
+        cmdPopd(directoryStack);
         return true;
     }
     else if ( strcmp(commandList[0], "dirs") == 0 ) {
+        cmdDirs(directoryStack);
         return true;
     }
     else if ( strcmp(commandList[0], "cd") == 0 ) {
+        cmdCd(commandList[1]);
         return true;
     }
 
     return false;
 }
 
+void cmdPushd(char* directory, vector<string>& directoryStack)
+{
+    size_t strSize = 100;
+    string strCurrDirectory = "";
+    bool loop = true;
+
+    while (loop) {
+        char* currentDirectory = (char *) malloc (strSize);
+
+        if (getcwd(currentDirectory, strSize) == currentDirectory) {
+            strCurrDirectory = currentDirectory;
+
+            if (cmdCd(directory) == true) {
+                directoryStack.push_back(strCurrDirectory);
+            }
+        }
+
+        free( currentDirectory );
+        loop = false;
+
+        // If path was too large to fit in allocated string using getcwd
+        // and size to allocate isn't too big, increase allocation size
+        if ((errno == ERANGE) && (strSize < 1000)) {
+            strSize *= 2;
+            loop = true;
+        }
+    }
+}
+
+void cmdPopd(vector<string>& directoryStack)
+{
+    if (directoryStack.empty()) {
+        cout << "Stack is empty" << endl;
+    }
+    else {
+        string directory = directoryStack.back();
+        directoryStack.pop_back();
+        cmdCd(directory.c_str());
+    }
+}
+
+void cmdDirs(vector<string>& directoryStack)
+{
+    for (unsigned int i = 0; i < directoryStack.size(); i++) {
+        cout << directoryStack[i] << endl;
+    }
+}
+
+bool cmdCd(const char* directory)
+{
+    //TODO: make sure argument is valid
+    //      may not need to, as chdir already checks
+    if ( chdir(directory) != 0) {
+        cout << "Unable to change directory" << endl;
+        return false;
+    }
+
+    return true;
+}
+
 void runProcess(char** commandList)
 {
     pid_t  pid;
-    /* fork another process */
+    // fork another process
     pid = fork();
 
-    if (pid < 0) { /* error occurred */
+    if (pid < 0) { // error occurred
         cerr << "Fork Failed" << endl;
         exit(-1);
     }
-    else if (pid == 0) { /* child process */
+    else if (pid == 0) { // child process
         execvp(commandList[0], commandList);
         cout << "Unknown Command" << endl;
         exit(0);
     }
-    else { /* parent process */
-        /* parent will wait for the child to complete */
+    else { // parent process
+        // parent will wait for the child to complete
         wait();
-        //exit(0);
     }
 }
